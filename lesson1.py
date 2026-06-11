cfd9f2d8f751e6b46534447865ca51e217892207
Bahareh.T <tavakolib7722@gmail.com>
2026-06-11 22:50:55 +0430

    Learning about numbers and lists

---
+++ lesson1.py
@@ -0,0 +1,37 @@
+#message = "Hello MS. Sharif, the uuniverse is waiting for you!"
+#print(message)
+
+#message = "Python is the Key to cosmos."
+#print(message)
+
+#MFW = "Herman Hesse"
+#print(MFW.title())
+
+#MFW = My Favorit Writer
+
+#print(f"""{MFW} once said: 
+#    'when I want to achive something,
+#    I'll become a rock,
+#      that is drowning into a lake
+#     I'll become my goal. as I see it
+#     drowning, I do as well. '""")
+#----------------------------------------------------------
+#C_Y = int("2026")
+#A = int(input("Which year did you were born?"))
+#print(f"your age is {C_Y - A} ")
+#----------------------------------------------------------
+my_favorit_languages = ['English', 'Persian', 'Korean', 'German', 'Chinese']
+print(f"""right now, I'm learning {my_favorit_languages[3]}
+and I'm really happy about that^^ """)
+my_favorit_languages.append('French')
+my_favorit_languages.insert(1,'Italian')
+print(my_favorit_languages)
+
+del my_favorit_languages[1]
+print(my_favorit_languages)
+
+popped_lang = my_favorit_languages.pop()
+print(popped_lang)
+#----------------------------------------------------------
+
+
